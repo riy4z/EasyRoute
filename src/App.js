25c6164d3ecea23aaaacf0defdd6aa0839a2fd69
Riyas Ahamed <56198819+riy4z@users.noreply.github.com>
2023-09-27 17:54:12 +0530

    Map and Geolocator Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import Papa from "papaparse";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>EasyRoute</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const style = {
+  width: "100%",
+  height: "100%",
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      map: null,
+      markers: [],
+    };
+  }
+
+  handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const csvData = event.target.result;
+        Papa.parse(csvData, {
+          header: true,
+          dynamicTyping: true,
+          complete: (result) => {
+            if (result && result.data) {
+              const addresses = result.data.map((row) => row["Street Address"]);
+              this.createPinsFromAddresses(addresses);
+            }
+          },
+        });
+      };
+
+      reader.readAsText(file);
+    }
+  };
+
+  createPinsFromAddresses = (addresses) => {
+    const { google } = this.props;
+    const geocoder = new google.maps.Geocoder();
+    const markers = [];
+
+    addresses.forEach((address) => {
+      geocoder.geocode({ address }, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK && results[0]) {
+          const location = results[0].geometry.location;
+          markers.push({
+            position: { lat: location.lat(), lng: location.lng() },
+          });
+          this.setState({ markers });
+        }
+      });
+    });
+  };
+
+  render() {
+    const { markers } = this.state;
+
+    return (
+      <>
+        <input type="file" accept=".csv" onChange={this.handleFileUpload} />
+        <Map
+          google={this.props.google}
+          style={style}
+          initialCenter={{
+            lat: 41.977226,
+            lng: -87.836723,
+          }}
+          zoom={14}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              position={marker.position}
+              name={`Marker ${index}`}
+            />
+          ))}
+        </Map>
+      </>
+    );
+  }
 }
 
-export default App;
+
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAnjRnY_9rEuXPhMHafgvZLflazp61NgO4",
+})(App);
